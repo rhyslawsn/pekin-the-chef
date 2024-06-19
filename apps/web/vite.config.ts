530f38fef9ed8f1d00f984ae63d62e6497131ef5
Rhys Lawson <rlawson5775@gmail.com>
2024-06-18 17:38:07 -0700

    refactor(web): another one

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
   build: {
     outDir: "build",
     sourcemap: true,
