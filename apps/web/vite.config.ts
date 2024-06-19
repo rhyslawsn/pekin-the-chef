5a05f7bce6146fd3803c4b2f27f777f06e2e67f7
Rhys Lawson <rlawson5775@gmail.com>
2024-06-18 17:19:29 -0700

    feature(web): update host to 0.0.0.0

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+  },
   build: {
     outDir: "build",
     sourcemap: true,
