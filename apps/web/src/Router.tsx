d675f3e581393f12bf1657815798c680cc073383
Rhys Lawson <rlawson5775@gmail.com>
2024-07-07 18:58:27 -0700

    refactor(web): change up routing structure

--- apps/web/src/Router.tsx
+++ apps/web/src/Router.tsx
@@ -5,8 +5,15 @@ import { Recipe } from "./screens/Recipe";
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:username/:slug" element={<Recipe />} />
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="/:username">
+          <Route index element={<Home />} />
+          <Route path="/:username/:slug">
+            <Route index element={<Recipe />} />
+          </Route>
+        </Route>
+      </Route>
     </Routes>
   );
 };
