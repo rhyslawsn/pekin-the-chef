2f133080dba04bde25bf4d622e6c94a66c9a3ef0
Rhys Lawson <rlawson5775@gmail.com>
2024-07-08 17:07:08 -0700

    refactor(web): routes back to normal

--- apps/web/src/Router.tsx
+++ apps/web/src/Router.tsx
@@ -1,17 +1,12 @@
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./screens/Home";
 import { Recipe } from "./screens/Recipe";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Outlet />}>
-        <Route index element={<Home />} />
-        <Route path=":username/:slug">
-          <Route index element={<Recipe />} />
-        </Route>
-      </Route>
-      <Route path="*" element={<div>Not Found</div>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/:username/:slug" element={<Recipe />} />
     </Routes>
   );
 };
